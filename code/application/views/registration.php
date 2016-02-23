9ce00bbaf51c234978678034e581968cd1a224c0
Alexander Quang <alexquang1991@gmail.com>
2016-02-22 19:13:32 -0800

    changed title on reg page

--- code/application/views/registration.php
+++ code/application/views/registration.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Log In</title>
+	<title>Registration</title>
 	<!-- jQuery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<!-- Latest compiled and minified CSS -->
