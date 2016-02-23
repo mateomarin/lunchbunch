57ab2fd73f33fc7fe8a880a4e61ad3835595bbb2
mateomarin <mateo.mw@gmail.com>
2016-02-22 18:37:33 -0800

    added addride view version

--- code/application/views/addride.php
+++ code/application/views/addride.php
@@ -3,6 +3,10 @@
   <head>
     <meta charset="utf-8">
     <title>Add a Ride</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
   </head>
   <body>
     <div class="container">
@@ -38,7 +42,9 @@
           <label for="duration">What is your takeout fee?</label>
           <input type="text" name="fee" value="">
         </div>
+        <input type="submit" value="Create Ride">
       </form>
+      <a href="#"><button type="button" name="Cancel">Cancel</button></a>
     </div>
   </body>
 </html>
