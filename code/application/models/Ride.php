6cb2c18385a1e3ce62a4833800cee6c5a4294b32
mateomarin <mateo.mw@gmail.com>
2016-02-24 18:43:55 -0800

    modified Ride model

--- code/application/models/Ride.php
+++ code/application/models/Ride.php
@@ -14,8 +14,8 @@ class Ride extends CI_Model {
      }
      function add_ride($ride)
      {
-         $query = "INSERT INTO Rides (destination_name, origin_coord, destination_coord, seats_avail, departure_time, distance, gas_fee, created_at, updated_at, timestamp, created_by, duration_id, accepts_order_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
-         $values = array($ride['destination_name'], $ride['origin_coord'], $ride['destination_coord'], $ride['seats_avail'], $ride['departure_time'], $ride['distance'], $ride['gas_fee'], date("Y-m-d, H:i:s"), date("Y-m-d, H:i:s"), $ride['timestamp'], $ride['duration_id'], $ride['accepts_order_id']);
+         $query = "INSERT INTO Rides (destination_name, destination_coord, seats_avail, departure_time, created_at, updated_at, duration_id, accepts_order_id) VALUES (?,?,?,?,?,?,?,?)";
+         $values = array($ride['destination_name'], $ride['destination_coord'], $ride['seats_avail'], $ride['departure_time'], date("Y-m-d, H:i:s"), date("Y-m-d, H:i:s"), $ride['duration_id'], $ride['accepts_order_id']);
          return $this->db->query($query, $values);
      }
 }
