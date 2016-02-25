7a11e9a18a17721475f5fe0916a86d68cb135491
mateomarin <mateo.mw@gmail.com>
2016-02-24 16:55:59 -0800

    created add_new_ride method in Rides controller

--- code/application/controllers/Rides.php
+++ code/application/controllers/Rides.php
@@ -10,6 +10,11 @@
     public function add_new_ride_page(){
       $this->load->view('addride');
     }
+    public function add_new_ride(){
+      $ride=$this->input->post();
+      $this->Ride->add_ride($ride);
+      redirect('/Users/success');
+    }
   }
 
 ?>
