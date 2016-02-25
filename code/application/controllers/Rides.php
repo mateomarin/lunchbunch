ca059687a1160ec771fe7daef224d0e2651dea6a
mateomarin <mateo.mw@gmail.com>
2016-02-24 16:33:00 -0800

    added Rides Controller

---
+++ code/application/controllers/Rides.php
@@ -0,0 +1,15 @@
+<?php
+  if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+  class Rides extends CI_Controller{
+    public function __construct(){
+      parent::__construct();
+      $this->output->enable_profiler();
+      $this->load->model('Ride');
+    }
+    public function add_new_ride(){
+      $this->load->view('addride');
+    }
+
+
+?>
