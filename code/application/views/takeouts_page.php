d789c25bbe8d2a7b346e4e0fa7bdfeb587aa8ac9
mateomarin <mateo.mw@gmail.com>
2016-02-25 17:25:36 -0800

    v1 takeouts

---
+++ code/application/views/takeouts_page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Takeouts Page</title>
+  </head>
+  <body>
+    <a href="/Takeouts/add_takeout_form/<?= $takeouts[0]['ride_id']?>"><button type="button" name="button">Add Takeout</button></a>
+    <?php foreach($takeouts as $takeout){?>
+      <p><?= $takeout['description']?></p>
+    <?php }?>
+  </body>
+</html>
