fa45f9d2d8e220da72408c24cd98eceb88149de8
Alexander Quang <alexquang1991@gmail.com>
2016-02-25 22:05:08 -0800

    tested place takeout

--- code/application/views/ridedetail.php
+++ code/application/views/ridedetail.php
@@ -53,7 +53,28 @@
                         </div>
                     </div>
                 </div>
-              <?php require('partials/takeoutmodal.php') ?>
+                  <div id="modal3" class="modal">
+                    <div class="modal-content">
+                        <div class="row">
+                            <form action="/Takeouts/add_takeout" class="col s12">
+                                <input type="hidden" name="ride_id" value="<?= $rides['id']?>">
+                                <input type="hidden" name="takeout_fee" value="<?= $rides['takeout_fee']?>">
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <textarea id="textarea1" name="description" class="materialize-textarea"></textarea>
+                                        <label for="textarea1">Write out your takeout order:</label>
+                                    </div>
+                                    <div class="input-field col s12">
+                                        <p>Driver's Fee: <?= $rides['takeout_fee']?></p>
+                                    </div>
+                                    <div class="input-field col s6">
+                                        <input class="btn" type="submit" name="name" value="Place Order">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
       </div>
     </body>
