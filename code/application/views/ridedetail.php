a266335d55f747bef22e450e96765d6d6175dd42
Alexander Quang <alexquang1991@gmail.com>
2016-02-25 21:27:49 -0800

    got the ride details page looking good

--- code/application/views/ridedetail.php
+++ code/application/views/ridedetail.php
@@ -1,214 +1,62 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Ride Details</title>
-    <!-- jQuery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<!--Import Google Icon Font-->
-	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<!-- Compiled and minified CSS -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
-	<!--Let browser know website is optimized for mobile-->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<!-- Compiled and minified JavaScript -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
-	<!-- Navigation JS -->
-	<script src="assets/js/nav.js"></script>
-	<!-- My CSS -->
-	<link rel="stylesheet" href="/assets/css/home.css">
-      <style>
-        html, body
-        {
-            height: 100%;
-            margin: 0;
-            padding: 0;
-        }
-        #map
-        {
-            height: 100%;
-            width: 50%;
-        }
-        #right-panel
-        {
-            font-family: 'Roboto','sans-serif';
-            line-height: 30px;
-            padding-left: 10px;
-        }
-
-        #right-panel select, #right-panel input
-        {
-            font-size: 15px;
-        }
-
-        #right-panel select
-        {
-            width: 100%;
-        }
-
-        #right-panel i
-        {
-            font-size: 12px;
-        }
-    </style>
-    <style>
-        #right-panel
-        {
-            float: right;
-            width: 48%;
-            padding-left: 2%;
-        }
-        #output
-        {
-            font-size: 11px;
-        }
-    </style>
-
-     <script>
-         $(document).load(function()
-            {
-                var distance = "";
-                var duration = "";
-
-                function initMap()
-                {
-                    var bounds = new google.maps.LatLngBounds;
-                    var markersArray = [];
-
-                    var selectedMode = DRIVING;
-                    var origin1 = "1980 Zanker Rd #30, San Jose, CA 95112";
-                    var destinationA = $array['destination'];
-
-                    var destinationIcon = 'https://chart.googleapis.com/chart?' + 'chst=d_map_pin_letter&chld=D|FF0000|000000';
-                    var originIcon = 'https://chart.googleapis.com/chart?' + 'chst=d_map_pin_letter&chld=O|FFFF00|000000';
-                    var map = new google.maps.Map(document.getElementById('map'),
-                        {
-                            center: {lat: 55.53, lng: 9.4},
-                            zoom: 10
-                        }
-                    );
-                    var geocoder = new google.maps.Geocoder;
-
-                    var service = new google.maps.DistanceMatrixService;
-                    service.getDistanceMatrix(
-                        {
-                            origins: [origin1],
-                            destinations: [destinationA],
-                            travelMode: google.maps.TravelMode[selectedMode],
-                            unitSystem: google.maps.UnitSystem.IMPERIAL,
-                            avoidHighways: false,
-                            avoidTolls: false
-                        }, function(response, status)
-                            {
-                                if (status !== google.maps.DistanceMatrixStatus.OK)
-                                {
-                                    alert('Error was: ' + status);
-                                }
-                                else
-                                {
-                                    var originList = response.originAddresses;
-                                    var destinationList = response.destinationAddresses;
-                                    var outputDiv = document.getElementById('output');
-                                    outputDiv.innerHTML = '';
-                                    deleteMarkers(markersArray);
-
-                                    var showGeocodedAddressOnMap = function(asDestination)
-                                        {
-                                            var icon = asDestination ? destinationIcon : originIcon;
-                                            return function(results, status)
-                                                {
-                                                    if (status === google.maps.GeocoderStatus.OK)
-                                                    {
-                                                        map.fitBounds(bounds.extend(results[0].geometry.location));
-                                                        markersArray.push(new google.maps.Marker(
-                                                            {
-                                                                map: map,
-                                                                position: results[0].geometry.location,
-                                                                icon: icon
-                                                            }));
-                                                    }
-                                                    else
-                                                    {
-                                                        alert('Geocode was not successful due to: ' + status);
-                                                    }
-                                                };
-                                        };
-
-                                    for (var i = 0; i < originList.length; i++)
-                                    {
-                                        var results = response.rows[i].elements;
-                                        geocoder.geocode({'address': originList[i]},
-                                        showGeocodedAddressOnMap(false));
-                                        for (var j = 0; j < results.length; j++)
-                                        {
-                                            geocoder.geocode(
-                                                {
-                                                    'address': destinationList[j]
-                                                },
-                                                    showGeocodedAddressOnMap(true));
-                                            outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] + ': ' + results[j].distance.text + ' in ' + results[j].duration.text + '<br>';
-                                            distance = results[j].distance.text;
-                                            duration = results[j].duration.text;
-                                        }
-                                    }
-
-                                }
-                            });
-                        return
-                    }
-
-                function deleteMarkers(markersArray)
-                {
-                  for (var i = 0; i < markersArray.length; i++)
-                  {
-                    markersArray[i].setMap(null);
-                  }
-                  markersArray = [];
-                }
-         });
-         document.getElementById("output").innerHTML = distance +  duration;
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDQroyo-GY-YEfdmHQANwVznL8Q2WTBT9s&signed_in=true&callback=initMap"async defer></script>
-
-  </head>
-  <body>
-<?php require('partials/navbar.php'); ?>
-    <div id="right-panel">
-      <div id="floating-panel">
-          <h1>Ride Details:</h1>
-          <div id="driver">
-            <img src="" alt="driver pic">
-            <div class="ride_details">
-                <ul>
-                    <li><?= $rides['destination_name'] ?></li>
-                    <li><?= $rides['seats_avail'] ?></li>
-                    <li><?= $rides['created_at'] ?></li>
-                    <li><?= $rides['accepts_order'] ?></li>
-                </ul>
+    <head>
+        <meta charset="utf-8">
+        <title>Ride Details</title>
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <!--Import Google Icon Font-->
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+        <!-- Navigation JS -->
+        <script src="/assets/js/nav.js"></script>
+        <!-- My CSS -->
+        <link rel="stylesheet" href="/assets/css/home.css">
+    </head>
+    <body>
+    <?php require('partials/navbar.php'); ?>
+        <div class="container">
+            <div class="row">
+                <div id="col s12">
+                    <div class="card large">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="/assets/img/pizza.jpg">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4"><?= $rides['destination_name'] ?><i class="material-icons right">more_vert</i></span>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4"><?= $rides['destination_name'] ?><i class="material-icons right">close</i></span>
+                            <ul>
+                                <li>Where to: <?= $rides['destination_name'] ?></li>
+                                <li>Number of Available Seats: <?= $rides['seats_avail'] ?></li>
+                                <li>Dining Status: <?php if ($rides['duration'] === 1) {
+                                    echo "Eat There";
+                                } else {
+                                    echo "Takeout";
+                                  } ?>
+                                </li>
+                                <li>Leaving Time: <?= $rides['departure_time'] ?></li>
+                                <li>Accepts Orders: <?php if ($rides['accepts_order'] == 1) {
+                                    echo "Yes";
+                                } else {
+                                    echo "No";
+                                  }  ?>
+                                </li>
+                            </ul>
+                            <a href="#"><button class="btn">Place Order</button></a>
+                        </div>
+                    </div>
+                </div>
+              <div class="ridedetail">
+                  
+              </div>
             </div>
-          </div>
-          <div class="ridedetail">
-              <ul>
-                  <li>Driver: <?= $details['driver'] ?></li>
-                  <li>Where to: <?= $details['destination_name'] ?></li>
-                  <li>Number of Available Seats: <?= $details['seats'] ?></li>
-                  <li>Dining Status: <?= $details['dining status'] ?></li>
-                  <li>Leaving Time: <?= $details['timestamp'] ?></li>
-                  <li>Accepts Orders: <?= $details['orders'] ?></li>
-                  <li>Contact Info: <?= $details['contact'] ?></li>
-              </ul>
-              <a href="#"><button>Reserve Seat</button></a>
-              <a href="#"><button>Place Order</button></a>
-          </div>
       </div>
-      <div>
-        <strong>Results</strong>
-      </div>
-      <div id="output"></div>
-        <p id="output"></p>
-        <script>
-
-        </script>
-      </div>
-      <div id="map"></div>
-  </body>
+    </body>
 </html>
