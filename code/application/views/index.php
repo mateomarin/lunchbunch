6159e2846fc7606d8cf1371b186731cdf52717cd
Alexander Quang <alexquang1991@gmail.com>
2016-02-26 09:29:08 -0800

    added center box to landing page

--- code/application/views/index.php
+++ code/application/views/index.php
@@ -21,9 +21,22 @@
 <body>
 	<?php require('partials/navbar2.php'); ?>
 	<div class="container">
-
+		<div class="row">
+			<div class="col s12 card-wrapper">
+				<div class="card grey lighten-3">
+					<div class="card-content black-text">
+						<span class="card-title">Have Lunch With Us</span>
+						<p>Connecting friends with lunch</p>
+					</div>
+					<div class="card-action">
+						<a class="waves-effect waves-light modal-trigger" href="#modal2">Sign Up</a>
+  						<a class="waves-effect waves-light modal-trigger" href="#modal1">Log In</a>
+					</div>
+				</div>
+			</div>
+		</div>
         <div class="fullscreen-bg">
-            <video loop muted autoplay poster="img/videoframe.jpg" id="bgvid">
+            <video loop muted autoplay id="bgvid">
                 <source src="assets/vid/MBV2.mp4" type="video/mp4">
             </video>
         </div>
